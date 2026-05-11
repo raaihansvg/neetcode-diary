9d663af2c587cac3e96790f39f7293287a798be3
raaihansvg <rlazuardi163@gmail.com>
2026-05-11 11:31:49 +0700

    lom

--- array/TopKFreq/TopKFreq.cpp
+++ array/TopKFreq/TopKFreq.cpp
@@ -4,6 +4,15 @@ using namespace std;
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        
+        int total = 0;
+        for(int i = 0; i < nums.size() -1; i ++){
+            for(int j = i + 1; j < nums.size(); j++){
+                if(nums[i] == nums[j]){
+                    total ++;
+                }
+            }
+        }  
     }
 };
+
+// BELUM KELAR AH TAR AJAWKWKKWKWKW
