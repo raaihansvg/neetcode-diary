accc2971387effc0dcd8988ac3575219bf0c4fb1
raaihansvg <rlazuardi163@gmail.com>
2026-05-17 14:12:33 +0700

    done

--- array/TopKFreq/TopKFreq.cpp
+++ array/TopKFreq/TopKFreq.cpp
@@ -1,18 +1,46 @@
-#include<vector>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        int total = 0;
-        for(int i = 0; i < nums.size() -1; i ++){
-            for(int j = i + 1; j < nums.size(); j++){
-                if(nums[i] == nums[j]){
-                    total ++;
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+
+        vector<int> angkaUnik(n);
+        vector<int> hitung(n);
+        vector<int> hasil(k);
+
+        int ukuran = 0;
+        int jumlah = 1;
+
+        for (int i = 1; i < n; i ++){
+            if (nums[i] == nums[i - 1]){
+                jumlah ++;
+            }else{
+                angkaUnik[ukuran] = nums[i - 1];
+                hitung[ukuran] = jumlah;
+                ukuran ++;
+                jumlah = 1;
+            }
+        }
+        angkaUnik[ukuran] = nums[n - 1];
+        hitung[ukuran] = jumlah;
+        ukuran ++;
+
+        for(int a = 0; a < k; a ++){
+            int maxIndex = 0;
+            for(int b = 1; b < ukuran; b ++){
+                if (hitung[b] > hitung[maxIndex]){
+                    maxIndex = b;
                 }
             }
-        }  
+
+            hasil[a] = angkaUnik[maxIndex];
+            hitung[maxIndex] = -1;
+        }
+        return hasil;
+        
     }
 };
-
-// BELUM KELAR AH TAR AJAWKWKKWKWKW
