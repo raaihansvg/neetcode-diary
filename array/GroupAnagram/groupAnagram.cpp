536eb1dba9b318b0d6dc39dc44ce5d83d1119985
raaihansvg <rlazuardi163@gmail.com>
2026-05-19 14:28:17 +0700

    groupAnagram

--- array/GroupAnagram/groupAnagram.cpp
+++ array/GroupAnagram/groupAnagram.cpp
@@ -1,1 +1,23 @@
-#include 
+#include<vector>
+#include<string>
+#include<unordered_map>
+#include<algorithm>
+using namespace std;
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> map;
+
+        for(string x : strs){
+            string simpan = x;
+            sort(simpan.begin(), simpan.end());
+            map[simpan].push_back(x);
+        }
+
+        vector<vector<string>> hasil;
+        for(auto x : map){
+            hasil.push_back(x.second);
+        }
+        return hasil;
+    }
+};
