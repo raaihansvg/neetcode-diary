dda6080d66d59d9b9e1525445d80459433928f93
raaihansvg <rlazuardi163@gmail.com>
2026-05-28 20:22:48 +0700

    drom1

--- twoPointer/palindrome.cpp
+++ twoPointer/palindrome.cpp
@@ -1,9 +1,16 @@
 using namespace std;
 #include <string>
 #include <iostream>
+#include <bits/stdc++.h>
 class Solution {
 public:
     bool isPalindrome(string s) {
-        
+        string rvrs = s;
+        reverse(rvrs.begin(), rvrs.end());
+        if(rvrs == s){
+            return true;
+        }else{
+            return false;
+        }
     }
 };
